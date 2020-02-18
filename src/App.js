e35bacc0da2351fbcd2d257c2653924b80e4e954
Michael Callahan <CallahanMichaelE@gmail.com>
2020-02-18 13:38:20 -0700

    updated App.js to import both SearchBar and PostsPage as well as added them into the App function to render

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ Not all files need code added.
 Look at each file to see where you need to pass props or add code 
 */
 import React from "react";
-
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import PostsPage from "./components/PostsContainer/PostsPage"
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 
@@ -14,6 +15,8 @@ const App = () => {
 
   return (
     <div className="App">
+      <SearchBar />
+      <PostsPage />
       {/* Add imported components here to render them */}
     </div>
   );
